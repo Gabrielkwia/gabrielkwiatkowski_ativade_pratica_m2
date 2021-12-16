b54f917aa6c9d9695d17b70d669f117738e28f50
Gabrielkwia <gabriel.kwiatkowski@aluno.unc.br>
2021-12-15 21:40:15 -0300

    Primeiro Commit

---
+++ app/Jogo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Jogo extends Model
+{
+    protected $table = "jogos";
+    
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CRUD - Voyager</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <style>
+            html, body {
+                
+                background-color: white;
+                color: black;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+			.m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .title {
+                font-size: 55px;
+            }
+
+			.title p{
+				margin-top: 0 !important;
+                margin-bottom: 50px
+            }
+
+			.title a{
+				color: black;
+			}
+
+			.title a:hover{
+				color: black;
+			}
+
+            span{
+                font-size: 30px;
+                display: block;
+            }
+
+            .negrito{
+                font-weight: bold;
+                text-decoration: underline;
+                margin-top: 35px;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    <p>Trabalho Individual M2 - Voyager/Laravel - Crud</p>
+                    <a style="font-size: 33px; text-decoration: none; margin-right: 18px;" href="/admin/">Clicar para a entrar e visualizar o Crud "Painel de Controle"</a>
+                    <br>
+                    <span class="negrito">Login</span>
+                    <span>Usuário: admin@admin.com</span>
+                    <span>Senha: password</span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html
